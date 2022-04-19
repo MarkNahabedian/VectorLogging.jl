ced6496a5ed52d96647737b61060d81ee7573817
Documenter.jl <documenter@juliadocs.github.io>
2022-04-19 23:24:12 +0000

    build based on c9d12bd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#VectorLogging.jl","page":"Home","title":"VectorLogging.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"VectorLOgging provides an in-memory logging destination.","category":"page"},{"location":"","page":"Home","title":"Home","text":"LogEntry\nVectorLogger","category":"page"}]
+}
